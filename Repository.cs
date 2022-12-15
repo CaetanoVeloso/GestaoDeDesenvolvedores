e5b830ad3c5ba5bc08e7a3ed8339ce6800ff5905
CaetanoVl <CaetanoVl@REVISION-PC>
2022-12-15 10:56:58 -0300

    inicio

---
+++ Repository.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoDeDesenvolvedores
+{
+    internal class Repository : DbContext
+    {
+        private static MySqlConnection _databaseConnection;
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public Repository() : base(GetDbConnection(), false)
+        {
+            // If database not exists, create it ...
+            if (Database.CreateIfNotExists())
+            {
+                // ... and...
+                Repository repositorio = this;
+
+                // ... insert a default administrator
+                Usuario administradorPadrao = new Usuario();
+                administradorPadrao.Nome = "Admin";
+
+                Credencial credencialPadrao = new Credencial();
+                credencialPadrao.Email = "admin@mail.com";
+                credencialPadrao.Senha = "xyz098";
+                credencialPadrao.Administrador = true;
+
+                credencialPadrao.Usuario = administradorPadrao;
+                administradorPadrao.Credencial = credencialPadrao;
+
+                repositorio.Usuarios.Add(administradorPadrao);
+                repositorio.SaveChanges();
+            }
+        }
+
+        public static MySqlConnection GetDbConnection()
+        {
+            if (_databaseConnection == null)
+            {
+                String connectionString = ConfigurationManager.ConnectionStrings["ProgVisConnectionString"].ConnectionString;
+                _databaseConnection = new MySqlConnection(connectionString);
+            }
+            return _databaseConnection;
+        }
+    }
+}
