7e36ff25531347f03bc4599a0f00ae67fb7654a8
CaetanoVl <CaetanoVl@REVISION-PC>
2022-12-15 15:48:02 -0300

    v2 - tardezinha

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GestaoDeDesenvolvedores
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.Text = "GestãoDeDesenvolvedores";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
